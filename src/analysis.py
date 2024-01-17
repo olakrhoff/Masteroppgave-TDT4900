3874b4037279131e37e7089f67327d472be35697
olakrhoff <ola.kr@hotmail.no>
2024-01-17 14:00:07 +0100

    Added more functionality to the analysis, number of nodes visited

--- src/analysis.py
+++ src/analysis.py
@@ -2,6 +2,7 @@ import os
 import matplotlib.pyplot as plt
 
 save_path = 'plots/'
+data_folder_path = 'data'
 
 def get_analysis_files(folder_path):
     try:
@@ -27,47 +28,75 @@ def get_analysis_files(folder_path):
 def analyse(file_path):
     print("Analysing file: " + file_path)
 
+    x_vals = [] # X-axis
+    y_vals = [] # Y-axis
+    nodes = []
+    node = []
     x = []
     y = []
-    a = []
+    a = [] # No. of agents for the lines
     prev_len = 1
     with open(file_path, 'r') as file:
         for line in file:
             seg = line.split(': ')
-            agents, goods, runs = seg[1].split(', ')
+            agents, goods, _ = seg[1].split(', ')
+            measurments = [float(x) for x in seg[2].split(', ')]
+            num_nodes = measurments[-1]
+            measurments = measurments[0:-1]
             if len(a) == 0 or agents != a[-1]:
                 if len(a) > 0:
-                    plt.plot(x, y, label='No. agents: ' + a[-1])
+                    #plt.plot(x, y, label='No. agents: ' + a[-1])
+                    x_vals.append(x)
+                    y_vals.append(y)
+                    nodes.append(node)
                     x = []
                     y = []
+                    node = []
                     prev_len = len(a)
     
                 a.append(agents)
             
             x.append(goods)
-            
-            measurments = [float(x) for x in seg[2].split(', ')]
             data = sum(measurments) / len(measurments)
             y.append(data)
+            node.append(num_nodes)
 
     # Plot the last line we detected
-    plt.plot(x, y, label='No. agents: ' + agents[-1])
+    #plt.plot(x, y, label='No. agents: ' + agents[-1])
+    x_vals.append(x)
+    y_vals.append(y)
+    nodes.append(node)
     
+    for i in range(len(x_vals)):
+        plt.plot(x_vals[i], y_vals[i], label='No. agents: ' + a[i])
+
     plt.xlabel('No. goods')
     plt.ylabel('Time (s)')
     plt.title(file_path.split('/')[-1])
     plt.legend()
     plt.savefig(save_path + file_path.split('/')[-1].split('.')[0] + ".png")
 
+    plt.close()
+
+
+    for i in range(len(x_vals)):
+        plt.plot(x_vals[i], nodes[i], label='No. agents: ' + a[i])
+
+
+    plt.xlabel('No. goods')
+    plt.ylabel('Nodes visited')
+    plt.title(file_path.split('/')[-1])
+    plt.legend()
+    plt.savefig(save_path + "nodes_" + file_path.split('/')[-1].split('.')[0] + ".png")
+
 
 def start_analysis():
     print("Starting analysis...")
-    data_folder_path = 'data'
     files = get_analysis_files(data_folder_path)
 
     for path in files:
         analyse(data_folder_path + '/' + path)
-     
+    
     print("Analysis done.")
 
 if __name__ == "__main__":
