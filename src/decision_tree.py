669d80795aaffadd48fadc795320575a86f5371d
olakrhoff <ola.kr@hotmail.no>
2024-02-19 15:08:15 +0100

    Test on mock data working

--- src/decision_tree.py
+++ src/decision_tree.py
@@ -1,28 +1,75 @@
+import sys
 from sklearn.datasets import load_iris
 from sklearn import tree
 from matplotlib import pyplot as plt
 import graphviz 
 
-iris = load_iris()
-X, y = iris.data, iris.target
-#print(X)
-#print(y)
-clf = tree.DecisionTreeClassifier()
-clf = clf.fit(X, y)
+def create_and_plot_tree(attribute_data_x, result_data_y, attribute_names=None, target_names=None):
+    clf = tree.DecisionTreeClassifier()
+    clf = clf.fit(attribute_data_x, result_data_y)
+    
+    tree.plot_tree(clf)
+    
+    dot_data = tree.export_graphviz(clf, out_file=None, 
+                                    feature_names=attribute_names,  
+                                    class_names=target_names,  
+                                    filled=True, rounded=True,  
+                                    special_characters=True)  
+    graph = graphviz.Source(dot_data)  
+    graph.render("decision_tree")
 
-tree.plot_tree(clf)
-#plt.show()
+def test():
+    iris = load_iris()
+    X, y = iris.data, iris.target
+    create_and_plot_tree(X, y, iris.feature_names, iris.target_names)
 
+def parse_file(filepath):
+    parsed = False
+    attribute_data_x = []
+    result_data_y = []
+    attribute_names = None
+    target_names = None
 
-print(iris.feature_names)
-print(iris.target_names)
-print("EXPORT")
-dot_data = tree.export_graphviz(clf, out_file=None, 
-                     feature_names=iris.feature_names,  
-                     class_names=iris.target_names,  
-                     filled=True, rounded=True,  
-                     special_characters=True)  
-print("EXPORTED")
-graph = graphviz.Source(dot_data)  
-graph.render("iris")
+    lines = 0
+    with open(filepath, 'r') as file:
+        for line in file:
+            parsed = True
+            lines += 1
+            print(line)
+            if (lines == 1):
+                attribute_names = line.split(', ')
+            elif (lines == 2):
+                target_names = line.split(', ')
+            else:
+                temp = line.split(', ')
+                attribute_data_x.append([float(val) for val in temp[0:-1]])
+                result_data_y.append(float(temp[-1]))
+
+    return parsed, attribute_data_x, result_data_y, attribute_names, None #target_names
+
+if __name__ == "__main__":
+    print("Creating decision tree...")
+
+    filename = None
+    if len(sys.argv) != 2:
+        print("Incorrect numbers of arguments passed, expected one argument")
+        print("python3 decision_tree.py <filepath>")
+        exit(1)
+    
+    filename = sys.argv[-1]
+
+    if (filename == "test"):
+        # The test runs the test code with the iris dataset
+        test()
+
+    else:
+        parsed, attribute_data_x, result_data_y, attribute_names, target_names = parse_file(filename)
+        if (not parsed):
+            print("Could not parse input file")
+            exit(1)
+
+        create_and_plot_tree(attribute_data_x, result_data_y, attribute_names, target_names)
+
+    print("done")
+
 
