82521d4d41b0e603e547e54cda58fdfe441bb010
olakrhoff <ola.kr@hotmail.no>
2024-02-22 11:08:13 +0100

    begun to create generate data

--- src/generate_dataset.cpp
+++ src/generate_dataset.cpp
@@ -1,10 +1,63 @@
+#include <cstdlib>
 #include <iostream>
 #include <vector>
+#include <getopt.h>
+
+using namespace std;
 
 
-int main()
+
+
+
+int NUMBER_OF_AGENTS {};
+
+void handle_options(int argc, char **argv)
 {
-    std::cout << "Generating data..." << std::endl;
+    int code {};
+    while ((code = getopt(argc, argv, "")) != -1)
+    {
+        switch (code)
+        {
+            default:
+                cout << "Parsing error for the options: " << (char)optopt << endl;
+                exit(EXIT_FAILURE);
+        }
+    }
+}
+
+
+
+vector<string> generate_data()
+{
+    vector<string> data {};
+
+    string temp {};
+    for (int goods = 0; goods < NUMBER_OF_GOODS; ++goods)
+        temp += get_weight() + " ";
+    data.emplace_back(temp);
+
+    for (int agents = 0; agents < NUMBER_OF_AGENTS; ++agents)
+    {
         
-    return 0;
+    }
+
+    return data;
+}
+
+void write_data_to_file(const vector<string> &data)
+{
+
+}
+
+int main(int argc, char **argv)
+{
+    cout << "Generating data..." << endl;
+
+    handle_options(argc, argv);
+
+    vector<string> data = generate_data();
+
+    write_data_to_file(data);
+
+    return EXIT_SUCCESS;
 }
