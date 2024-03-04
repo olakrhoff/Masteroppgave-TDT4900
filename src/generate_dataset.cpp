7207c9ae34d073f64eda0a5a7a58f0aa0b99456c
olakrhoff <ola.kr@hotmail.no>
2024-03-04 15:04:44 +0100

    Added interval generation skeleton

--- src/generate_dataset.cpp
+++ src/generate_dataset.cpp
@@ -1,4 +1,5 @@
 #include <cstdlib>
+#include <random>
 #include <iostream>
 #include <fstream>
 #include <vector>
@@ -68,6 +69,7 @@ int M_OVER_N_RATIO_HIGH {};
 int BUDGET_USED_PERCENT_LOW {};
 int BUDGET_USED_PERCENT_HIGH {};
 
+int INTERVALS {};
 string FILE_OUTPUT_PATH {};
 ATTRIBUTES_T interval_option {NONE};
 
@@ -105,7 +107,7 @@ void handle_options(int argc, char **argv)
             case 'i':
                 {
                     char interval = *optarg;
-                    
+                       
                     switch (interval)
                     {
                         case 'a':
@@ -130,6 +132,9 @@ void handle_options(int argc, char **argv)
                             cout << "Interval value not recognised" << endl;
                             exit(EXIT_FAILURE);
                     }
+                    
+                    string temp = (optarg + 1); // We skip the char at the start
+                    INTERVALS = stoi(temp);
 
                     break;
                 }
@@ -206,30 +211,30 @@ uint64_t get_good()
     return 2;
 }
 
-vector<string> generate_data()
+vector<string> generate_data(const int number_of_goods, const int number_of_agents)
 {
     vector<string> data {};
 
     string temp {};
     // Generate the weights for the goods
-    for (int goods = 0; goods < NUMBER_OF_GOODS; ++goods)
+    for (int goods = 0; goods < number_of_goods; ++goods)
     {
         temp += to_string(get_weight());
-        if (goods + 1 < NUMBER_OF_GOODS)
+        if (goods + 1 < number_of_goods)
             temp += " ";
     }
     data.emplace_back(temp);
 
     // Generate the capacity for the agent and values for the goods
-    for (int agents = 0; agents < NUMBER_OF_AGENTS; ++agents)
+    for (int agents = 0; agents < number_of_agents; ++agents)
     {
         temp = "";
         temp += to_string(get_capacity()) + ": ";
         
-        for (int goods = 0; goods < NUMBER_OF_GOODS; ++goods)
+        for (int goods = 0; goods < number_of_goods; ++goods)
         {
             temp += to_string(get_good());
-            if (goods + 1 < NUMBER_OF_GOODS)
+            if (goods + 1 < number_of_goods)
                 temp += " ";
         }
         data.emplace_back(temp);
@@ -254,15 +259,100 @@ void write_data_to_file(const vector<string> &data)
     file.close();
 }
 
+/*
+ *  This function is used to generate a random value in an interval drawn from
+ *  a uniform distribution.
+ */
+int get_random_number_from_interval(int lower_bound, int upper_bound)
+{
+    random_device rd;
+    mt19937 generator(rd());
+
+    uniform_int_distribution<> distribution(lower_bound, upper_bound);
+
+    return distribution(generator);
+}
+
+/*
+ * This function gets values in an interval split into equal steps. Such that at
+ * step 0 we get the lower bound and at step 'number_of_iterations' we get
+ * the upper bound
+ */
+double get_value_in_interval(int lower_bound, int upper_bound, int iteration, int number_of_iterations)
+{
+    if (number_of_iterations < 2)
+    {
+        cout << "Expect at least two iterations in an interval" << endl;
+        exit(EXIT_FAILURE);
+    }
+    double diff = (double)(upper_bound - lower_bound);
+
+    double val_per_itr = diff / (number_of_iterations - 1);
+
+    return lower_bound + (val_per_itr * iteration);
+}
+
 int main(int argc, char **argv)
 {
     handle_options(argc, argv);
     validate_options();
     
     cout << "Generating data..." << endl;
-    vector<string> data = generate_data();
+    if (interval_option == NONE)
+    {
+        int number_of_goods = get_random_number_from_interval(NUMBER_OF_GOODS_LOW, NUMBER_OF_GOODS_HIGH);
+        int number_of_agents = get_random_number_from_interval(NUMBER_OF_AGENTS_LOW, NUMBER_OF_AGENTS_HIGH);
+
+        vector<string> data = generate_data(number_of_goods, number_of_agents);
     
-    write_data_to_file(data);
+        write_data_to_file(data);
+    }
+    else
+    {
+        for (int i = 0; i < INTERVALS; ++i)
+        {
+            string temp_name = FILE_OUTPUT_PATH;
+            FILE_OUTPUT_PATH += "_" + to_string(i);
+            int number_of_goods = get_random_number_from_interval(NUMBER_OF_GOODS_LOW, NUMBER_OF_GOODS_HIGH);
+            int number_of_agents = get_random_number_from_interval(NUMBER_OF_AGENTS_LOW, NUMBER_OF_AGENTS_HIGH);
+            double avg_permutation_distance = get_random_number_from_interval(PERMUTATION_DISTANCE_LOW, PERMUTATION_DISTANCE_HIGH);
+            double avg_value_distance = get_random_number_from_interval(VALUE_DISTANCE_LOW, VALUE_DISTANCE_HIGH);
+            double m_over_n = get_random_number_from_interval(M_OVER_N_RATIO_LOW, M_OVER_N_RATIO_HIGH);
+            double budget_used_percent = get_random_number_from_interval(BUDGET_USED_PERCENT_LOW, BUDGET_USED_PERCENT_HIGH);
+
+            switch (interval_option)
+            {
+                case NONE:
+                    cout << "Control flow should not take us here" << endl;
+                    exit(EXIT_FAILURE);
+                case AGENTS:
+                    number_of_agents = get_value_in_interval(NUMBER_OF_AGENTS_LOW, NUMBER_OF_AGENTS_HIGH, i, INTERVALS);
+					break;
+                case GOODS:
+                    number_of_goods = get_value_in_interval(NUMBER_OF_GOODS_LOW, NUMBER_OF_GOODS_HIGH, i, INTERVALS);
+					break;
+                case AVG_PERMUTATION_DISTANCE:
+                    avg_permutation_distance = get_value_in_interval(PERMUTATION_DISTANCE_LOW, PERMUTATION_DISTANCE_HIGH, i, INTERVALS);
+					break;
+                case AVG_VALUE_DISTANCE:
+                    avg_value_distance = get_value_in_interval(VALUE_DISTANCE_LOW, VALUE_DISTANCE_HIGH, i, INTERVALS);
+					break;
+                case M_OVER_N:
+                    m_over_n = get_value_in_interval(M_OVER_N_RATIO_LOW, M_OVER_N_RATIO_HIGH, i, INTERVALS);
+					break;
+                case BUDGET_USED_PERCENT:
+                    budget_used_percent = get_value_in_interval(BUDGET_USED_PERCENT_LOW, BUDGET_USED_PERCENT_HIGH, i, INTERVALS);
+					break;
+                default:
+                    cout << "Could not recognise interval option" << endl;
+                    exit(EXIT_FAILURE);
+            }
+
+            vector<string> data = generate_data(number_of_goods, number_of_agents);
+            write_data_to_file(data);
+            FILE_OUTPUT_PATH = temp_name;
+        }
+    }
     
     cout << "Data generated" << endl;
 
