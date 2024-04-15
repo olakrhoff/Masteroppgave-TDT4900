a2fc2293762f01daf0572c269bc762b9e9151761
olakrhoff <ola.kr@hotmail.no>
2024-04-15 15:50:59 +0200

    Fixed

--- run_batch.sh
+++ run_batch.sh
@@ -1,4 +1,4 @@
-#!/bin/zsh
+#!/bin/bash
 
 
 folder="data/intervals/M/small"
@@ -8,10 +8,19 @@ counter=$(find "data/results" -type f | wc -l)
 # We hack the format to be a number, otherwise it adds a lot of spaces on the first element
 ((counter++))
 ((counter--))
+
+
+optimisations=(-m -n -u -b
+               -n\ -u -n\ -b)
+
  
 find "$folder" -type f | while read -r file; do
-    ./src/bin/BBCMMS -d "$file" -e "data/results/data$counter.txt" -m
-    ((counter++))
+    for opt in "${optimisations[@]}"; do
+        echo "OPT: $opt"
+        echo "./src/bin/BBCMMS -d $file -e data/results/data$counter.txt $opt"
+        ./src/bin/BBCMMS -d "$file" -e "data/results/data$counter.txt" $opt
+        ((counter++))
+    done
 done
 
 
