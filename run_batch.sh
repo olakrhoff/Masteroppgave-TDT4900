7f101b76daebeca62c205b7b0c3c80eb016ace3e
olakrhoff <ola.kr@hotmail.no>
2024-04-15 14:10:25 +0200

    bug fix

--- run_batch.sh
+++ run_batch.sh
@@ -4,11 +4,14 @@
 folder="data/intervals/M/small"
 
 counter=$(find "data/results" -type f | wc -l)
+
+# We hack the format to be a number, otherwise it adds a lot of spaces on the first element
+((counter++))
+((counter--))
  
 find "$folder" -type f | while read -r file; do
-    echo "$counter, FILE: $file"
-    ((counter++))
     ./src/bin/BBCMMS -d "$file" -e "data/results/data$counter.txt" -m
+    ((counter++))
 done
 
 
