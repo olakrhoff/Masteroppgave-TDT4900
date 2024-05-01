58dfe1a645a120c69907ba97187c07a08c1f02f8
olakristofferhoff <ola.kr@hotmail.no>
2024-05-01 14:30:52 +0200

    Fixed bug where all agents get zero MMS

--- src/BBCMMS.cpp
+++ src/BBCMMS.cpp
@@ -1012,7 +1012,7 @@ tuple<double, double, state_t, bool> upper_bound_find_MMS(const agent_t &agent, 
 double find_MMS(const agent_t &agent, uint64_t num_agents, const vector<weight_t> &weights)
 {
     uint64_t num_goods = agent.goods.size();
-
+    cout << "AGNETS: " << num_agents << endl;
 
     // --- PREPROCESS THE BEST SOLUTION ---
     vector<agent_t> agents {};
@@ -1592,15 +1592,16 @@ pair<allocation_t, double> BBCMMS(const vector<agent_t> &agents,
                                               const vector<weight_t> &weights)
 {
     double nodes_visited {};
-    int num_goods = (int)agents.at(0).goods.size();
     int num_agents = (int)agents.size();
-    
+    if (num_agents == 0)
+        return {allocation_t(0), 0.0};
+    int num_goods = (int)agents.at(0).goods.size();
+    cout << "BBCMMS: " << num_agents << endl;  
     vector<double> agents_MMS {};
     for (int agent_idx = 0; agent_idx < (int)num_agents; ++agent_idx)
     {
         cout << "Finding MMS for agent " << agent_idx + 1 << endl;
         agents_MMS.emplace_back(find_MMS(agents.at(agent_idx), num_agents, weights));
-
         // --- HANDLE THE CASE WHERE WE GET A ZERO MMS VALUE ---
         // We simply remove the first one that got a zero value and solve without it
         if (agents_MMS.back() == 0)
@@ -1612,6 +1613,7 @@ pair<allocation_t, double> BBCMMS(const vector<agent_t> &agents,
             allocation_t allocation(num_agents - 1);
             double alpha_MMS {};
 
+            cout << "HERE: " << num_agents << endl;
             tie(allocation, alpha_MMS) = BBCMMS(reduced_agents, weights);
            
             allocation_t corrected_allocation(num_agents);
